b954358f32a80dd14cf9cdad0c3cfaf47b125a53
weiguowei <mailforwgw@gmail.com>
2026-08-17 17:17:09 +0800

    feat: ChatClient的使用和结构化输出

---
+++ 01_springai-alibaba-quick-start/src/main/java/com/wgw/controller/ZhipuChatClientController.java
@@ -0,0 +1,85 @@
+package com.wgw.controller;
+
+import com.wgw.model.Book;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.zhipuai.ZhiPuAiChatOptions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chatClient")
+public class ZhipuChatClientController {
+
+    private final ChatClient chatClient;
+
+    public ZhipuChatClientController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+//
+//    @GetMapping("/simple")
+//    public String simple(@RequestParam(name = "query") String query) {
+//        ZhiPuAiChatOptions chatOptions = new ZhiPuAiChatOptions();
+//        chatOptions.setModel("glm-4.6"); //设置模型
+//        SystemMessage systemMessage = new SystemMessage("你是一个ai助手");
+//        UserMessage userMessage = new UserMessage(query);
+//        //构建prompt
+//        Prompt prompt = new Prompt(List.of(systemMessage, userMessage), chatOptions);
+//        return chatClient.prompt(prompt)
+//                .call()
+//                .content();
+//    }
+
+    @GetMapping("/simple")
+    public String simple(@RequestParam(name = "query") String query) {
+        ZhiPuAiChatOptions chatOptions = ZhiPuAiChatOptions.builder()
+                .model("glm-4.6")
+                .temperature(0.0)
+                .maxTokens(15536)
+                .build();
+
+        return chatClient.prompt()
+                .system("你是一个ai助手")
+                .user(query)
+                .options(chatOptions)
+                .call().content();
+    }
+
+    @GetMapping("/book")
+    public Book book() {
+        ZhiPuAiChatOptions chatOptions = ZhiPuAiChatOptions.builder()
+                .model("glm-4.6")
+                .temperature(0.0)
+                .maxTokens(15536)
+                .build();
+
+        return chatClient.prompt()
+                .user("给我随机生成一本书，要求书名和作者都是中文")
+                .options(chatOptions)
+                .call()
+                .entity(Book.class);
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream(@RequestParam(name = "query") String query) {
+        ZhiPuAiChatOptions chatOptions = ZhiPuAiChatOptions.builder()
+                .model("glm-4.6")
+                .temperature(0.0)
+                .maxTokens(15536)
+                .build();
+
+        return chatClient.prompt()
+                .system("你是一个ai助手")
+                .user(query)
+                .options(chatOptions)
+                .stream().content();
+    }
+
+}
---
+++ 01_springai-alibaba-quick-start/src/main/java/com/wgw/model/Book.java
@@ -0,0 +1,13 @@
+package com.wgw.model;
+
+import lombok.Data;
+
+/**
+ * 结构化输出
+ */
+@Data
+public class Book {
+    private String name;
+
+    private String author;
+}
